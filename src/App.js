fc66aed883ef09cb5daa5341ab76a9a387d89ae9
Kunal Singhdeo <singhdeokunal11@gmail.com>
2021-11-07 13:56:05 +0530

    Deleting tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+let globalID = 0;
+
 function App() {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState([]);
@@ -8,8 +10,12 @@ function App() {
     e.preventDefault();
     setTodos((oldTodos) => {
       setTask("");
-      return [...oldTodos, task];
+      return [...oldTodos, { todo: task, id: globalID++ }];
     });
+  }
+
+  function deleteItem(itemID) {
+    setTodos((oldTodos) => oldTodos.filter((item) => item.id !== itemID));
   }
 
   return (
@@ -24,8 +30,15 @@ function App() {
         <button type="submit">Create Todo</button>
       </form>
       <ul>
-        {todos.map((todo, index) => {
-          return <li>{todo}</li>;
+        {todos.map((item, index) => {
+          return (
+            <div key={item.id}>
+              <li>
+                {item.todo}({item.id})
+              </li>
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
+            </div>
+          );
         })}
       </ul>
     </div>
